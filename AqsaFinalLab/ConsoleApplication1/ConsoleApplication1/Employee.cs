570f7a8891e4fe9f304169d6c0bb722a4ad42fe6
AqsaBano330 <64906746+AqsaBano330@users.noreply.github.com>
2021-11-30 12:26:30 +0500

    Add files via upload

---
+++ AqsaFinalLab/ConsoleApplication1/ConsoleApplication1/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+
+{
+    
+   
+         
+
+    {
+        private int Employee_ID;
+        private int Employee_Name;
+        private int Employee_Type;
+        private int Hours_Worked;
+        private int No_Of_Unit_Produced;
+
+
+}
